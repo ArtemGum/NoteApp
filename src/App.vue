b8fcc6e3319c0c87703c067476ee2a593b98cd4b
ArtemGum <45288500+ArtemGum@users.noreply.github.com>
2019-05-13 23:06:35 +0500

    add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,140 +1,46 @@
 <template>
 <div class="wrapper">
-  <div class="wrapper-content">
-    <section>
-      <div class="container">
 
-        <div class="note-header" style="margin: 36px 0; justify-content: center;">
+	<header>
+		<div class="navbar">
+			<div class="container">
+				<div class="navbar-content">
+					<div class="logo">Vue-cli</div>
+					<ul class="navbar-list">
+						<li class="navbar-item" v-for="link in links" :key="link.title">
+							<router-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</router-link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
 
-          <!-- title -->
-          <p> {{ title }} </p>
+	<router-view></router-view>
 
-        </div>
+	<!-- <footer></footer> -->
 
-        <!-- message -->
-        <message v-if="message" :message="message"/>
-
-        <!-- new note -->
-        <newNote :note="note" @addNote="addNote"/>
-
-
-        <div class="note-header" style="margin: 36px 0;">
-
-          <!-- title -->
-          <h1> {{ title }} </h1>
-
-          <!-- search -->
-          <search
-          :value="search"
-          placeholder="Find your note"
-          @search="search = $event"/>
-
-          <!-- icons controls -->
-          <div class="icon">
-            <svg :class="{ active: grid }" @click="grid = true" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
-            <svg :class="{ active: !grid }" @click="grid = false" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
-          </div>
-        </div>
-
-        <!-- note list -->
-        <notes :notes="notesFilter" :grid="grid" @remove="removeNote"/>
-
-      </div>
-    </section>
-  </div>
 </div>
 </template>
 
 <script>
-import message from '@/components/Message.vue'
-import notes from '@/components/Notes.vue'
-import newNote from '@/components/NewNote.vue'
-import search from '@/components/Search.vue'
-
 export default {
-  components: {
-    message, notes, newNote, search
-  },
-  data () {
-    return {
-        title: 'Notes App',
-        search: '',
-        message: null,
-        grid: true,
-        note: {
-          title: '',
-					descr: '',
-					priority:'standart'
-        },
-        notes: [
-          {
-            title: 'First Note',
-						descr: 'Description for first note',
-						priority: 'standart',
-            date: new Date(Date.now()).toLocaleString()
-          },
-          {
-            title: 'Second Note',
-						descr: 'Description for second note',
-						priority: 'important',
-            date: new Date(Date.now()).toLocaleString()
-          },
-          {
-            title: 'Third Note',
-						descr: 'Description for third note',
-						priority: 'veryImportant',
-            date: new Date(Date.now()).toLocaleString()
-          }
-        ]
-     }
-   },
-   computed: {
-     notesFilter () {
-      let array = this.notes,
-          search = this.search
-      if (!search) return array
-      // Small
-      search = search.trim().toLowerCase()
-      // Filter
-      array = array.filter(function (item) {
-        if (item.title.toLowerCase().indexOf(search) !== -1) {
-          return item
-        }
-      })
-      // Error
-      return array
-     }
-   },
-   methods: {
-      addNote () {
-        // console.log(this.note)
-        let {title, descr, priority} = this.note;
-
-        if (title === '') {
-          this.message = 'title can`t be blank!'
-          return false
-        }
-
-        this.notes.push({
-          title,
-					descr,
-					priority,
-          date: new Date(Date.now()).toLocaleString()
-        })
-        this.message = null
-        this.note.title = ''
-				this.note.descr = ''
-				this.note.priority = 'standart'
-    },
-    removeNote (index) {
-      this.notes.splice(index, 1)
-    }
-  }
+	data () {
+		return {
+			links: [
+				{ title: 'Home', url: '/' },
+				{ title: 'Example', url: '/example' }
+			]
+		}
+	}
 }
 </script>
 
-<style scoped>
-.container {
-  max-width: 800px;
+<style lang="scss">
+.navbar-link {
+	&.router-link-exact-active {
+		color: #5247e7;
+	}
 }
+
 </style>
